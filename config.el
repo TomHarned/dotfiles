a53738ee69087bb1c3762a8f6fd5fc3c5c785ded
Tom Harned <thomas.harned@gmail.com>
2021-02-03 22:26:46 -0500

    update configs

--- config.el
+++ config.el
@@ -6,8 +6,6 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "Tom Harned"
-      user-mail-address "thomas.harned@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -16,11 +14,10 @@
 ;; + `doom-variable-pitch-font'
 ;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
 ;;   presentations or streaming.
-;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font (font-spec :family "monospace" :size 36 :weight 'semi-light)
+      doom-variable-pitch-font (font-spec :family "sans" :size 16))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -35,6 +32,177 @@
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
+;; Use Spacemacs-style "SPC-SPC" for M-x
+(map! :leader
+      :desc "Open M-x like spacemacs" "SPC" #'counsel-M-x)
+
+(map! :leader
+      :desc "Run a shell command" ":" #'shell-command)
+
+;; Python repl shortcuts
+(map! :leader
+      :desc "Send python statement" "mss" #'python-shell-send-statement)
+
+(map! :leader
+      :desc "Send python region" "msr" #'python-shell-send-region)
+
+(map! :leader
+      :desc "Send python function" "msf" #'python-shell-send-defun)
+
+(map! :leader
+      :desc "Send python file" "msF" #'python-shell-send-defun)
+
+;; Break lines at 80 chars
+(add-hook 'text-mode-hook #'auto-fill-mode)
+(setq-default fill-column 80)
+(add-hook 'prog-mode-hook #'auto-fill-mode)
+
+;; Reduce python crashes
+;; There are bunch of bugs that make python freeze in emacs
+;; This seems to get rid of them
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args "--simple-prompt -i")
+
+;;(add-hook 'inferior-python-mode-hook
+;;          (lambda ()
+;;            (setq company-mode nil))
+
+;; show python/jupyter plots inline
+(setq ein:output-area-inlined-images t)
+(after! ein
+  (setq ein:slice-image nil))
+
+(after! mu4e
+  ;; mu4e email settings
+  (setq mu4e-root-maildir "~/.mail"
+        mu4e-attachment-dir "~/Downloads"
+        mu4e-sent-folder "/Sent"
+        mu4e-drafts-folder "/Drafts"
+        mu4e-trash-folder "/Trash"
+        mu4e-refile-folder "/Archive")
+
+  (setq user-full-name "Tom Harned"
+        user-mail-address "thomas.harned@protonmail.com")
+
+  ;; Get mail
+  (setq mu4e-get-mail-command "mbsync protonmail"
+        mu4e-change-filenames-when-moving t   ; needed for mbsync
+        mu4e-update-interval 120)             ; update every 2 minutes
+
+  ;; Send mail
+  (setq message-send-mail-function 'smtpmail-send-it
+        smtpmail-auth-credentials "~/.authinfo.gpg"
+        smtpmail-smtp-server "127.0.0.1"
+        smtpmail-stream-type 'starttls
+        smtpmail-smtp-service 1025
+        )
+  (add-to-list 'gnutls-trustfiles (expand-file-name "~/.config/protonmail/bridge/cert.pem"))
+  (setq mu4e-html2text-command "html2text -utf8 -width 72")
+  )
+;; Set venv
+;; assumes venv is in .virtualenvs
+;;
+
+(defun setvenv (venv)
+  ;; the "s" means it's taking a string
+  (interactive "s")
+  (setq path
+        (concat
+         "~/.virtualenvs/"
+         venv
+         "/"))
+  (pyvenv-activate path))
+
+
+
+
+;; Run crontab using a emacs buffer as editor instead of launching the
+;; system editor
+(defun crontab-e ()
+  "Run `crontab -e' in a emacs buffer."
+  (interactive)
+  (with-editor-async-shell-command "crontab -e"))
+
+(defun sh-send-line-or-region (&optional step)
+  (interactive ())
+  (let ((proc (get-process "shell"))
+        pbuf min max command)
+    (unless proc
+      (let ((currbuff (current-buffer)))
+        (shell)
+        (switch-to-buffer currbuff)
+        (setq proc (get-process "shell"))))
+    (setq pbuff (process-buffer proc))
+    (if (use-region-p)
+        (setq min (region-beginning)
+              max (region-end))
+      (setq min (point-at-bol)
+            max (point-at-eol)))
+    (setq command (concat (buffer-substring min max) "\n"))
+    (with-current-buffer pbuff
+      (goto-char (process-mark proc))
+      (insert command)
+      (move-marker (process-mark proc) (point)))
+       ;;pop-to-buffer does not work with save-current-buffer -- bug?
+    (process-send-string  proc command)
+    (display-buffer (process-buffer proc) t)
+    (when step
+      (goto-char max)
+      (next-line))))
+
+
+(defun sh-send-line-or-region-and-step ()
+  (interactive)
+  (sh-send-line-or-region t))
+(defun sh-switch-to-process-buffer ()
+  (interactive)
+  (pop-to-buffer (process-buffer (get-process "shell")) t))
+
+;;(define-key sh-mode-map [(control ?j)] 'sh-send-line-or-region-and-step)
+;;(define-key sh-mode-map [(control ?c) (control ?z)] 'sh-switch-to-process-buffer)
+
+;; SAME function as above, but for vterm instead of shell
+(defun vt-send-line-or-region (&optional step)
+  (interactive ())
+  (let ((proc (get-process "vterm"))
+        pbuf min max command)
+    (unless proc
+      (let ((currbuff (current-buffer)))
+        (vterm)
+        (switch-to-buffer currbuff)
+        (setq proc (get-process "vterm"))
+        ))
+    (setq pbuff (process-buffer proc))
+    (if (use-region-p)
+        (setq min (region-beginning)
+              max (region-end))
+      (setq min (point-at-bol)
+            max (point-at-eol)))
+    (setq command (concat (buffer-substring min max) "\n"))
+    (with-current-buffer pbuff
+      (read-only-mode -1)
+      (goto-char (process-mark proc))
+      (insert command)
+      (move-marker (process-mark proc) (point))
+      ) ;;pop-to-buffer does not work with save-current-buffer -- bug?
+    (process-send-string  proc command)
+    (display-buffer (process-buffer proc) t)
+    (when step
+      (goto-char max)
+      (next-line))
+    ))
+
+(defun vt-send-line-or-region-and-step ()
+  (interactive)
+  (vt-send-line-or-region t))
+(defun vt-switch-to-process-buffer ()
+  (interactive)
+  (pop-to-buffer (process-buffer (get-process "vterm")) t))
+
+;;TODO: figure out how these work for the shell version
+;;and then assign keys
+;;(define-key vt-mode-map [(control ?j)] 'sh-send-line-or-region-and-step)
+;;(define-key sh-mode-map [(control ?c) (control ?z)] 'sh-switch-to-process-buffer)
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
--- init.vim
+++ init.vim
@@ -15,7 +15,23 @@ set cmdheight=2
 set previewheight=4
 set hidden
 set textwidth=80
+" let g:onedark_termcolors =  16
 
+"Use 24-bit (true-color) mode in Vim/Neovim when outside tmux.
+"If you're using tmux version 2.2 or later, you can remove the outermost $TMUX check and use tmux's 24-bit color support
+"(see < http://sunaku.github.io/tmux-24bit-color.html#usage > for more information.)
+if (empty($TMUX))
+  if (has("nvim"))
+    "For Neovim 0.1.3 and 0.1.4 < https://github.com/neovim/neovim/pull/2198 >
+    let $NVIM_TUI_ENABLE_TRUE_COLOR=1
+  endif
+  "For Neovim > 0.1.5 and Vim > patch 7.4.1799 < https://github.com/vim/vim/commit/61be73bb0f965a895bfb064ea3e55476ac175162 >
+  "Based on Vim patch 7.4.1770 (`guicolors` option) < https://github.com/vim/vim/commit/8a633e3427b47286869aa4b96f2bfc1fe65b25cd >
+  " < https://github.com/neovim/neovim/wiki/Following-HEAD#20160511 >
+  if (has("termguicolors"))
+    set termguicolors
+  endif
+endif
 " This has to be before plugins so that coc and ale can play together
 let g:ale_disable_lsp = 1
 " let g:ale_completion_enabled = 1
@@ -28,7 +44,7 @@ Plug 'tpope/vim-fugitive'
 Plug 'airblade/vim-gitgutter'
 
 " show file tree
-Plug 'scrooloose/nerdtree'
+" Plug 'scrooloose/nerdtree'
 Plug 'preservim/nerdcommenter'
 Plug 'preservim/tagbar'
 
@@ -37,11 +53,13 @@ Plug 'junegunn/fzf', { 'dir': '~/.fzf', 'do': './install --all' }
 Plug 'junegunn/fzf.vim'
 
 " Colorthemes
+Plug 'joshdick/onedark.vim'
 Plug 'NLKNguyen/papercolor-theme'
 Plug 'tomasr/molokai'
 Plug 'sainnhe/sonokai'
 Plug 'morhetz/gruvbox'
 Plug 'altercation/vim-colors-solarized'
+Plug 'romgrk/doom-one.vim'
 
 " Nice airline format at bottom of screen
 Plug 'vim-airline/vim-airline'
@@ -56,7 +74,16 @@ Plug 'elzr/vim-json'
 Plug 'sheerun/vim-polyglot'
 
 " Clojure Plugins if I ever get around to learning
-Plug 'Olical/conjure'
+Plug 'Olical/conjure', {'tag': 'v4.3.1'}
+
+" Conjure support - jack-in with nrepl dependencies
+Plug 'tpope/vim-dispatch'
+Plug 'clojure-vim/vim-jack-in'
+
+" Only in Neovim:
+Plug 'radenling/vim-dispatch-neovim'
+" Plug 'clojure-vim/vim-jack-in'
+" Plug 'Olical/conjure'
 Plug 'tpope/vim-surround'
 Plug 'guns/vim-sexp'
 Plug 'tpope/vim-sexp-mappings-for-regular-people'
@@ -78,6 +105,9 @@ Plug 'dense-analysis/ale'
 " F--ing linting
 Plug 'plasticboy/vim-markdown'
 Plug 'Vimjas/vim-python-pep8-indent'
+
+" Syntax
+Plug 'sheerun/vim-polyglot'
 call plug#end()
 
 " configure fzf
@@ -116,7 +146,7 @@ let g:tagbar_ctags_bin="/usr/bin/ctags"
 
 let g:ale_linters = {'python': ['autopep8'],'clojure': ['cli-kondo', 'joker'],'markdown': ['markdownlint']}
 " Configure shortcut for nerdtree toggle
-nnoremap <Space>n :NERDTreeToggle<CR>
+" nnoremap <Space>n :NERDTreeToggle<CR>
 
 " Configure shortcut for tagbar toggle
 nnoremap <Space>t :TagbarToggle<CR>
@@ -124,8 +154,17 @@ nnoremap <Space>t :TagbarToggle<CR>
 " Remap ctrl-w to space for split nav
 nnoremap <Space> <C-w>
 
-nnoremap <Space> <Tab> :bp<CR>
+nnoremap <Space><Tab> :bp<CR>
 
+" Make escape work on neovim terminal
+tnoremap <Esc> <C-\><C-n>
+tnoremap <M-[> <Esc>
+
+" Enable some sort of escape functionality on neovim terminal command line
+tnoremap <C-v><Esc> <Esc>
+
+" Remap tag-base 'jump to definion'
+nnoremap gd <C-]>
 
 " Appearance
 syntax enable
@@ -137,15 +176,16 @@ set noswapfile
 
 " set completeopt-=menu,preview
 " set completeopt+=menuone
-let g:coc_user_config = {}
-let g:coc_user_config['coc.preferences.jumpCommand'] = ':SplitIfNotOpen4COC'
+" let g:coc_user_config = {}
+" let g:coc_user_config['coc.preferences.jumpCommand'] = ':SplitIfNotOpen4COC'
+syntax on
+colorscheme onedark 
+let g:airline_theme='onedark'
 
-" colorscheme
-autocmd VimEnter * colorscheme PaperColor
-autocmd VimEnter * AirlineTheme gruvbox
-set background=dark 
+" set background=dark 
+" autocmd VimEnter * colorscheme doom-one
+" autocmd VimEnter * AirlineTheme gruvbox
 
-" let g:airline_theme='gruvbox'
 
 
 " Show just the filename
@@ -156,4 +196,6 @@ let g:airline#extensions#tagbar#enabled = 1
 let g:airline#extensions#tabline#enabled = 1
 let g:airline#extensions#hunks#enabled=1
 
+let g:airline_exclude_filetypes = ['nerdtree']
 
+"set maplocalleader=","
